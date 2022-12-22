68de88af2ac9e0ba0813dedaffd58f64159d8e1b
surendra singh <90984357+Surendrasingh2003@users.noreply.github.com>
2022-12-22 11:39:42 +0530

    commit

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,16 +1,55 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import { useParams } from "react-router-dom";
 import { apiGet } from "../misc/config";
 const Show = () => {
+  const intialState = {
+    show: null,
+    isLoading: true,
+    error: null,
+  };
+
+  const reducer = (prevState, action) => {
+    switch (action.type) {
+      case "FETCH_SUCCESS": {
+        return { isLoading: false, error: null, show: action.show };
+      }
+      case "FETCH_FAILURE": {
+        return { ...prevState, isLoading: false, error: action.error };
+      }
+      default:
+        return prevState;
+    }
+  };
+
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    intialState
+  );
+
   const { id } = useParams();
-  const [show, setShow] = useState(null);
+  // const [show, setShow] = useState(null);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
+
   useEffect(() => {
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then((r) => r.json())
       .then((results) => {
-        setShow(results);
+        setTimeout(() => {
+          dispatch({ type: "FETCH_SUCCESS", show: results });
+        }, 2000);
+      })
+      .catch((err) => {
+        dispatch({ type: "FETCH_FAILURE", err: err.message });
       });
   }, [id]);
+  console.log("show", show);
+  if (isLoading) {
+    return <div>Data is being Loading</div>;
+  }
+  if (error) {
+    return <div>Error occured : {error.message}</div>;
+  }
 
   return <div>this is Show page</div>;
 };
